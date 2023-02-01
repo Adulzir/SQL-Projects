8da631552454062f69aa61c12c42aae6f3e76498
Erika <adulzir.altamirano@gmail.com>
2023-01-31 20:40:47 -0800

    Update CovidProject1.sql

--- CovidProject/CovidProject1.sql
+++ CovidProject/CovidProject1.sql
@@ -1,6 +1,7 @@
 /*
 Covid 19 Data Exploration
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+Raw data provided by Our World in Data: https://ourworldindata.org/covid-deaths - Downloaded Nov 28 2022
 */
 
 SELECT * from Project1.CovidDeaths
